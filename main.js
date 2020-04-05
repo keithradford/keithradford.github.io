b2912b57d5d4b04cbd9ec7d067e45e282a0fac54
Keith Radford <khr8771@gmail.com>
2020-04-04 17:23:21 -0700

    Disabled scrolling while nav menu is open

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+function noScroll() {
+    window.scrollTo(0, 0);
+}
+
 $(document).ready(function(){
     $('.menu-toggler').on('click', function(){
+        let nav = document.getElementById('nav-menu');
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+        if ( nav.classList.contains('open')){
+            window.addEventListener('scroll', noScroll);
+        }
+        else {
+            window.removeEventListener('scroll', noScroll);
+        }
     });
 
     $('.top-nav .nav-link').on('click', function(){
         $('.menu-toggler').removeClass('open');
         $('.top-nav').removeClass('open');
+        window.removeEventListener('scroll', noScroll);
     });
 
     $('nav a[href*="#"]').on('click', function(){
