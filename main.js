7da2e14ff9e8502e6baf2d7d1a30a5ddfb49a6cf
Keith Radford <khr8771@gmail.com>
2020-05-13 20:59:23 -0700

    Added comments to main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const body = document.getElementsByTagName('BODY')[0];
 
 $(document).ready(function () {
+
+    // Disable scrolling when nav menu is entered
     $('.menu-toggler').on('click', function () {
         let nav = document.getElementById('nav-menu');
         $(this).toggleClass('open');
@@ -13,18 +15,21 @@ $(document).ready(function () {
         }
     });
 
+    // Exit nav menu (Enable scroll and remove class 'open')
     $('.top-nav .nav-link').on('click', function () {
         $('.menu-toggler').removeClass('open');
         $('.top-nav').removeClass('open');
         body.style.overflow = 'visible';
     });
 
+    // Set scrollbar position to location of selected section in nav bar
     $('nav a[href*="#"]').on('click', function () {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
         }, 2000);
     });
 
+    // Set scrollbar to top of page
     $('#up').on('click', function () {
         $('html, body').animate({
             scrollTop: 0
