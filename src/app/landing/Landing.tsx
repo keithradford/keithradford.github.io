0b4ee11a09beb38b0194f6ec4e6298d143ef4524
Keith Radford <khr8771@gmail.com>
2021-03-24 19:43:22 -0700

    add landing page

--- src/app/landing/Landing.tsx
+++ src/app/landing/Landing.tsx
@@ -1,48 +1,77 @@
-import {
-  Container,
-  Image,
-  HStack,
-  Text,
-  Heading,
-  Box,
-  VStack,
-  Flex,
-} from "@chakra-ui/react";
+import { Heading, Flex, IconButton, HStack, Spacer } from "@chakra-ui/react";
+import { Link as Scroll } from "react-scroll";
+import { VscChevronDown } from "react-icons/vsc";
+import { FaLinkedinIn, FaGithubAlt } from "react-icons/fa";
 
 export function Landing(): JSX.Element | null {
   return (
-    <Container
-      maxW="100%"
+    <Flex
+      justifyContent="center"
+      alignItems="center"
       h="100vh"
-      bg="black"
-      bgImg={`url(${process.env.PUBLIC_URL + "/assets/background.jpg"})`}
       bgRepeat="no-repeat"
       bgSize="cover"
-      centerContent
+      bgImg={`url(${process.env.PUBLIC_URL + "/assets/background.jpg"})`}
+      flexDirection="column"
+      color="white"
     >
-      <Flex h="100vh" alignItems="center" justifyContent="center">
-        <HStack
-          direction="row"
-          spacing="24px"
-          pr="4"
-          bg="rgba(255, 255, 255, 0.80)"
+      <HStack mt="1rem">
+        <Heading
+          as="h1"
+          fontSize="4.5rem"
+          fontFamily="Open Sans"
+          fontWeight="bold"
+          mb="1rem"
+          mr="1rem"
         >
-          <Image
-            boxSize="200px"
-            objectFit="cover"
-            src="https://i.ibb.co/q0KgLvp/keith.png"
-            alt="Keith Radford"
-          />
-          <VStack alignItems="left" w="430px" justifyContent="center">
-            <Heading size="4xl" fontFamily="VT323" flexGrow={1}>
-              keith radford
-            </Heading>
-            <Heading size="xl" fontFamily="VT323">
-              software engineering student
-            </Heading>
-          </VStack>
-        </HStack>
-      </Flex>
-    </Container>
+          Keith Radford
+        </Heading>
+        <IconButton
+          id="social"
+          aria-label="github"
+          icon={<FaGithubAlt />}
+          size="lg"
+          isRound
+          colorScheme="whiteAlpha"
+          fontSize="30px"
+          as="a"
+          href="https://github.com/keithradford"
+          target="_blank"
+        />
+        <Spacer />
+        <IconButton
+          id="social"
+          aria-label="linkedin"
+          icon={<FaLinkedinIn />}
+          size="lg"
+          isRound
+          colorScheme="whiteAlpha"
+          fontSize="30px"
+          as="a"
+          href="https://www.linkedin.com/in/keith-radford/"
+          target="_blank"
+        />
+      </HStack>
+      <Heading
+        fontSize="1.3rem"
+        fontWeight="bold"
+        mt="0"
+        fontFamily="Open Sans"
+      >
+        Software Engineering Student at the University of Victoria
+      </Heading>
+      <Scroll to="projects" smooth={true}>
+        <IconButton
+          mt="2.3rem"
+          aria-label="scroll-button"
+          icon={<VscChevronDown />}
+          size="lg"
+          isRound
+          colorScheme="whiteAlpha"
+          id="down"
+          fontSize="30px"
+        />
+      </Scroll>
+    </Flex>
   );
 }
