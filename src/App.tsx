3c6b9104db5ffd49c84f1077c54acb1d2ddb25a9
Keith Radford <khr8771@gmail.com>
2021-03-28 18:42:21 -0700

    add about section

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { Landing } from "./app/landing/Landing";
 import { Projects } from "./app/projects/Projects";
 import { Work } from "./app/work/Work";
 import { Box } from "@chakra-ui/react";
+import { About } from "./app/about/About";
 
 export function App(): JSX.Element | null {
   return (
-    <>
+    <Box bg="#353535">
       <Landing />
-      <Box minH="100vh" bg="#353535" id="info">
+      <About />
+      <Box minH="100vh" id="info">
         <Projects />
         <Work />
       </Box>
-    </>
+      <footer style={{ textAlign: "center", color: "whitesmoke" }}>
+        &copy; 2021 Keith Radford
+      </footer>
+    </Box>
   );
 }
